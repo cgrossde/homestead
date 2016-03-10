624eb24198a15656d5f79c5811dd0fec922b3f84
Chris <gross@blubyte.de>
2016-03-10 13:33:50 +0100

    Update serve-hhvm.sh

--- scripts/serve-hhvm.sh
+++ scripts/serve-hhvm.sh
@@ -38,5 +38,5 @@ block="server {
 echo "$block" > "/etc/nginx/sites-available/$1"
 ln -fs "/etc/nginx/sites-available/$1" "/etc/nginx/sites-enabled/$1"
 service nginx restart
-service php5-fpm restart
+service php-fpm7.0 restart
 service hhvm restart
